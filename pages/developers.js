0a214065523b604d862b85b2a13b7b847fb2dda6
reyyymartt <reymartcenteno03@gmail.com>
2024-09-13 00:51:03 +0800

    update developers.js

--- pages/developers.js
+++ pages/developers.js
@@ -2,10 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const devs = [
- {id:1,name:'Rey',des:'Sup lil nigs',img:'/assets/me.jpg'},
- {id:2,name:'Jasper',des:'Jullyver bading',img:null},
- {id:3,name:'Jed',img:null},
- {id:4,name:'Regie',img:null}
+ {
+  id:1,
+  name:'Regie',
+  des:'Sup lil nigs',
+  img:null,
+  pos:'UI Designer'
+ },
+ {
+  id:2,
+  name:'Jasper',
+  des:'Jullyver bading',
+  img:null,
+  pos:'Developer'
+ },
+ {
+  id:3,
+  name:'Jed',
+  img:null,
+  pos:null
+ },
+ {
+  id:4,
+  name:'Rey',
+  img:null,
+  pos:'Lead Developer'
+ }
 ]
 
 class MainPage extends React.Component{
@@ -13,10 +35,20 @@ class MainPage extends React.Component{
  
   render (){
   
+  const imgs = {
+   width:'100px',
+   height:'100px',
+   objectFit:'cover'
+  }
+  
   const devslist = devs.map(dev=>
-  <div key={dev.id} className="text-center card-body border rounded m-1 bg-light shadow-sm col-sm-5">
-  <i className="card-title"><h3>{dev.name}</h3></i>
+  <div key={dev.id} className="media card-body border rounded m-1 bg-light shadow-sm col-lg-1">
+  <img style={imgs} className="rounded" justify-self-center src={dev.img?dev.img:'./assets/user.png'}></img>
+  <div className="media-body m-2">
+  <i className="card-title"><h4>{dev.name}<span className="badge shadow m-1 badge-info">{dev.pos?dev.pos:''}</span></h4></i>
+  <hr/>
   <p className="card-text">{dev.des?dev.des:'no description provided'}</p>
+  </div>
   </div>
   )
   
