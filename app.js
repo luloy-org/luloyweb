87e5ff2098efb6c8712a53bf6f95f1308aec537b
reyyymartt <reymartcenteno03@gmail.com>
2024-09-19 19:11:03 +0800

    update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import HomePage from './pages/home'
 import AboutPage from './pages/about'
 import DevsPage from './pages/developers'
 
+
+
 import bootstrap from 'bootstrap'
 
 const content = document.getElementById('content')
